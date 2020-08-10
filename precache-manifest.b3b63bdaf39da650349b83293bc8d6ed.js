11b4b726b8eb4d8f23c091f6d8fd18cea85369d9
Dylan Kennedy <kennedymysword@aol.com>
2020-08-10 05:57:41 -0400

    Updates

---
+++ precache-manifest.b3b63bdaf39da650349b83293bc8d6ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd9f68f22c3261c01306ee87edb5f91b",
+    "url": "/Recipes/index.html"
+  },
+  {
+    "revision": "be522e953ad323e64a85",
+    "url": "/Recipes/static/css/main.e9eb9c22.chunk.css"
+  },
+  {
+    "revision": "095714d5e7f7d15df2ee",
+    "url": "/Recipes/static/js/2.c71727f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Recipes/static/js/2.c71727f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be522e953ad323e64a85",
+    "url": "/Recipes/static/js/main.a5049b5a.chunk.js"
+  },
+  {
+    "revision": "e12db099ca5af2a8ce76",
+    "url": "/Recipes/static/js/runtime-main.ea483e6a.js"
+  }
+]);
